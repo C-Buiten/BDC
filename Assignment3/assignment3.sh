bd2ab5de88f253cbc69196a37c395692e96b2fb8
cvanbuiten <c.van.buiten@st.hanze.nl>
2023-06-27 13:46:02 +0200

    Swapped assignment3 to 2021 version for consistency.
    (Windows - Untested)

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+#SBATCH --job-name=BDC_Assignment3
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
+#SBATCH --mail-user=c.van.buiten@st.hanze.nl
+#SBATCH --mail-type=ALL
+#SBATCH --partition=assemblix
 
-source activate /commons/conda/dsls;
 
-export FILE="/commons/Themas/Thema12/HPC/rnaseq.fastq";
+source /commons/conda/conda_load.sh
+export data=/data/dataprocessing/MinIONData/all.fq
+export ref=/data/dataprocessing/MinIONData/all_bacteria.fna
 
 
-python3 assignment3.py -n 16 $FILE
+cores=16
+
+for i in $(seq 1 $cores);
+do
+  /usr/bin/time -o timings.txt --append -f "${i}\t%e" minimap2 $ref $data -t "${i}" -a > /dev/null
+done
 
